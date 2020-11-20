7db841ba17b459e44046132acbd690474e1c5b0c
James <jamese.richardson@autotrader.co.uk>
2020-11-20 09:43:40 +0000

    Adding a queue for handling IO between dropbox and DB

--- src/main/java/ebird2postgres/ebird/EBirdReader.java
+++ src/main/java/ebird2postgres/ebird/EBirdReader.java
@@ -6,32 +6,67 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.atomic.AtomicReference;
 
+import com.univocity.parsers.common.processor.BatchedColumnProcessor;
 import com.univocity.parsers.tsv.TsvParser;
 import com.univocity.parsers.tsv.TsvParserSettings;
 
 public class EBirdReader {
-	
+
+	private static final int MAX_QUEUE_SIZE = 100;
+
 	private final AtomicReference<String> lastRowRead = new AtomicReference<String>();
 
 	private final InputStream ebird;
-	
+
+	private final BlockingQueue<String[]> queue = new LinkedBlockingDeque<>(MAX_QUEUE_SIZE);
+
+	private final ExecutorService executorService = Executors.newFixedThreadPool(3);
+
 	public EBirdReader(final InputStream ebird) {
 		this.ebird = ebird;
 	}
 	
 	public void read(final EBirdLocalityPredicate predicate, final EBirdRecordHandler recordHandler, final EBirdErrorHandler errorHandler) {
+		startReading(errorHandler);
+		startWriting(predicate, recordHandler, errorHandler);
+	}
+
+	private void startReading(final EBirdErrorHandler errorHandler) {
 		TsvParserSettings settings = new TsvParserSettings();
 		settings.setHeaderExtractionEnabled(true);
 		TsvParser parser = new TsvParser(settings);
-		
-		try (InputStream is = new BufferedInputStream(ebird)) {
-			parser.iterate(is, "UTF-8").forEach(row -> handleRow(row, predicate, recordHandler, errorHandler));
-		} catch (IOException e) {
-			errorHandler.handleError(lastRowRead.get(), null, e);
-		}
-	}	
+
+		executorService.submit(() -> {
+			try (InputStream is = new BufferedInputStream(ebird)) {
+				parser.iterate(is, "UTF-8").forEach(row -> {
+					try {
+						queue.put(row);
+					} catch (InterruptedException e) {
+						errorHandler.handleError(lastRowRead.get(), row, e);
+					}
+				});
+			} catch (IOException e) {
+				errorHandler.handleError(lastRowRead.get(), null, e);
+			}
+			System.out.println("Finished reading file.");
+		});
+	}
+
+	private void startWriting(final EBirdLocalityPredicate predicate, final EBirdRecordHandler recordHandler, final EBirdErrorHandler errorHandler) {
+		executorService.submit(() -> {
+			try {
+				handleRow(queue.take(), predicate, recordHandler, errorHandler);
+			} catch (InterruptedException e) {
+				errorHandler.handleError(lastRowRead.get(), null, e);
+			}
+		});
+	}
 
 	private void handleRow(final String[] row, final EBirdLocalityPredicate predicate, final EBirdRecordHandler recordHandler, final EBirdErrorHandler errorHandler) {
 		final String previousRow = lastRowRead.getAndSet(row[0]);
